e4e07998cec7d3ebea62d6e4fdf4130375bfe2ea
Tracy <tracycobrien@gmail.com>
2019-07-17 19:27:40 -0400

    removes app.get

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ if (process.env.NODE_ENV === 'production') {
 	console.log('producion');
 	app.use(express.static('client/build'));
 }
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"),{ useNewUrlParser: true };
